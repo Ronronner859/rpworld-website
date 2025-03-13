1bb2e024d22c05bcfc5d4270b532c2b1e98bae21
Beak <chenyilong916002@gmail.com>
2025-03-13 17:19:23 +0800

    fix build bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,49 @@
-import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
-  //是否构建ssr
-  ssr: true,
+    nitro: {
+        preset: "vercel",
+    },
+    ssr: true,
 
-  build: {
-      transpile: ['vuetify'],
-  },
+    build: {
+        transpile: ['vuetify'],
+    },
 
-  css: [
-      '~/assets/css/global.css'
-  ],
+    css: [
+        '~/assets/css/global.css'
+    ],
 
-  modules: [
-      '@nuxtjs/tailwindcss',
-      '@pinia/nuxt',
-      '@pinia-plugin-persistedstate/nuxt',
-      '@nuxtjs/i18n',
-      (_options, nuxt) => {
-          nuxt.hooks.hook('vite:extendConfig', (config) => {
-              // @ts-expect-error
-              config.plugins.push(vuetify({autoImport: true}))
-          })
-      },
-      //...
-  ],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/i18n',
+        (_options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) => {
+                // @ts-expect-error
+                config.plugins.push(vuetify({ autoImport: true }))
+            })
+        },
+        //...
+    ],
 
-  i18n: {
-      strategy: 'no_prefix', // 添加路由前缀的方式
-      locales: ["en", "zh", "es","fr","de","it"], //配置语种
-      defaultLocale: 'zh', // 默认语种
-      vueI18n: './i18n/config.ts', // 通过vueI18n配置
-  },
+    i18n: {
+        strategy: 'no_prefix', // 添加路由前缀的方式
+        locales: ["en", "zh", "es", "fr", "de", "it"], //配置语种
+        defaultLocale: 'zh', // 默认语种
+        vueI18n: './i18n/config.ts', // 通过vueI18n配置
+    },
 
-  vite: {
-      vue: {
-          template: {
-              transformAssetUrls,
-          }
-          ,
-      }
-      ,
-  },
+    vite: {
+        vue: {
+            template: {
+                transformAssetUrls,
+            }
+            ,
+        }
+        ,
+    },
 
-  compatibilityDate: '2025-03-12',
+    compatibilityDate: '2025-03-12',
 })
